06ac591c7bd1c9544ccc7c04f7a06528f73fb73c
lanzhihao <276810003@qq.com>
2018-09-05 13:43:00 +0800

    ssd1306 128x32 I2C driverand example

--- Adafruit_SSD1306.h
+++ Adafruit_SSD1306.h
@@ -1,20 +1,20 @@
 /*********************************************************************
-This is a library for our Monochrome OLEDs based on SSD1306 drivers
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
-
-These displays use SPI to communicate, 4 or 5 pins are required to  
-interface
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
-*********************************************************************/
+ This is a library for our Monochrome OLEDs based on SSD1306 drivers
+ 
+ Pick one up today in the adafruit shop!
+ ------> http://www.adafruit.com/category/63_98
+ 
+ These displays use SPI to communicate, 4 or 5 pins are required to
+ interface
+ 
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+ 
+ Written by Limor Fried/Ladyada  for Adafruit Industries.
+ BSD license, check license.txt for more information
+ All text above, and the splash screen must be included in any redistribution
+ *********************************************************************/
 
 
 #include "application.h"
@@ -24,40 +24,41 @@ All text above, and the splash screen must be included in any redistribution
 #define BLACK 0
 #define WHITE 1
 
-#define SSD1306_I2C_ADDRESS   0x3C	// 011110+SA0+RW - 0x3C or 0x3D
+#define SSD1306_I2C_ADDRESS   0x3C    // 011110+SA0+RW - 0x3C or 0x3D
+//#define SSD1306_I2C_ADDRESS   0x3D    // 011110+SA0+RW - 0x3C or 0x3D
 // Address for 128x32 is 0x3C
 // Address for 128x64 is 0x3D (default) or 0x3C (if SA0 is grounded)
 
 /*=========================================================================
-    SSD1306 Displays
-    -----------------------------------------------------------------------
-    The driver is used in multiple displays (128x64, 128x32, etc.).
-    Select the appropriate display below to create an appropriately
-    sized framebuffer, etc.
-
-    SSD1306_128_64  128x64 pixel display
-
-    SSD1306_128_32  128x32 pixel display
-
-    -----------------------------------------------------------------------*/
-#define SSD1306_128_64
-//#define SSD1306_128_32
+ SSD1306 Displays
+ -----------------------------------------------------------------------
+ The driver is used in multiple displays (128x64, 128x32, etc.).
+ Select the appropriate display below to create an appropriately
+ sized framebuffer, etc.
+ 
+ SSD1306_128_64  128x64 pixel display
+ 
+ SSD1306_128_32  128x32 pixel display
+ 
+ -----------------------------------------------------------------------*/
+//#define SSD1306_128_64
+#define SSD1306_128_32
 /*=========================================================================*/
 
 #if defined SSD1306_128_64 && defined SSD1306_128_32
-  #error "Only one SSD1306 display can be specified at once in SSD1306.h"
+#error "Only one SSD1306 display can be specified at once in SSD1306.h"
 #endif
 #if !defined SSD1306_128_64 && !defined SSD1306_128_32
-  #error "At least one SSD1306 display must be specified in SSD1306.h"
+#error "At least one SSD1306 display must be specified in SSD1306.h"
 #endif
 
 #if defined SSD1306_128_64
-  #define SSD1306_LCDWIDTH                  128
-  #define SSD1306_LCDHEIGHT                 64
+#define SSD1306_LCDWIDTH                  128
+#define SSD1306_LCDHEIGHT                 64
 #endif
 #if defined SSD1306_128_32
-  #define SSD1306_LCDWIDTH                  128
-  #define SSD1306_LCDHEIGHT                 32
+#define SSD1306_LCDWIDTH                  128
+#define SSD1306_LCDHEIGHT                 32
 #endif
 
 #define SSD1306_SETCONTRAST 0x81
@@ -107,42 +108,42 @@ All text above, and the splash screen must be included in any redistribution
 #define SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL 0x2A
 
 class Adafruit_SSD1306 : public Adafruit_GFX {
- public:
-  Adafruit_SSD1306(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST, int8_t CS);
-  Adafruit_SSD1306(int8_t DC, int8_t RST, int8_t CS);
-  Adafruit_SSD1306(int8_t RST);
-
-  void begin(uint8_t switchvcc = SSD1306_SWITCHCAPVCC, uint8_t i2caddr = SSD1306_I2C_ADDRESS);
-  void ssd1306_command(uint8_t c);
-  void ssd1306_data(uint8_t c);
-
-  void clearDisplay(void);
-  void invertDisplay(uint8_t i);
-  void display();
-
-  void startscrollright(uint8_t start, uint8_t stop);
-  void startscrollleft(uint8_t start, uint8_t stop);
-
-  void startscrolldiagright(uint8_t start, uint8_t stop);
-  void startscrolldiagleft(uint8_t start, uint8_t stop);
-  void stopscroll(void);
-
-  void dim(bool dim);
-
-  void drawPixel(int16_t x, int16_t y, uint16_t color);
-
-  virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
-  virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
-
- private:
-  int8_t _i2caddr, _vccstate, sid, sclk, dc, rst, cs;
-  void fastSPIwrite(uint8_t c);
-
-  boolean hwSPI;
-
-  inline void drawFastVLineInternal(int16_t x, int16_t y, int16_t h, uint16_t color) __attribute__((always_inline));
-  inline void drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color) __attribute__((always_inline));
-
+public:
+    Adafruit_SSD1306(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST, int8_t CS);
+    Adafruit_SSD1306(int8_t DC, int8_t RST, int8_t CS);
+    Adafruit_SSD1306(int8_t RST);
+    
+    void begin(uint8_t switchvcc = SSD1306_SWITCHCAPVCC, uint8_t i2caddr = SSD1306_I2C_ADDRESS);
+    void ssd1306_command(uint8_t c);
+    void ssd1306_data(uint8_t c);
+    
+    void clearDisplay(void);
+    void invertDisplay(uint8_t i);
+    void display();
+    
+    void startscrollright(uint8_t start, uint8_t stop);
+    void startscrollleft(uint8_t start, uint8_t stop);
+    
+    void startscrolldiagright(uint8_t start, uint8_t stop);
+    void startscrolldiagleft(uint8_t start, uint8_t stop);
+    void stopscroll(void);
+    
+    void dim(bool dim);
+    
+    void drawPixel(int16_t x, int16_t y, uint16_t color);
+    
+    virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
+    virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+    
+private:
+    int8_t _i2caddr, _vccstate, sid, sclk, dc, rst, cs;
+    void fastSPIwrite(uint8_t c);
+    
+    boolean hwSPI;
+    
+    inline void drawFastVLineInternal(int16_t x, int16_t y, int16_t h, uint16_t color) __attribute__((always_inline));
+    inline void drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color) __attribute__((always_inline));
+    
 };
 
 
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-link https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf
+SSD1306 datasheet: https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf
